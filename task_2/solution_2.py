2001d0b3d6edad4b70481b9cc9bdf9d0ceb30c0c
MDAhse <44751798+MDAhse@users.noreply.github.com>
2020-10-22 22:40:07 +0300

    Update solution_2.py

--- task_2/solution_2.py
+++ task_2/solution_2.py
@@ -1,4 +1,12 @@
-# your code here
 user_input = input()
-# your code here
-print(most_frequent_character)
+b = user_input[0]
+for i in user_input:
+	if i not in b:
+		b = b+i
+n = 0
+c = ''
+for i in b:
+	if user_input.count(i) > n:
+		n=user_input.count(i)
+		c = i
+print(c)
