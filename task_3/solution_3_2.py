be800581f8e2997cac6e62c048da9389c405470a
MDAhse <44751798+MDAhse@users.noreply.github.com>
2020-10-28 02:05:28 +0300

    Update solution_3_2.py

--- task_3/solution_3_2.py
+++ task_3/solution_3_2.py
@@ -3,8 +3,8 @@ n = 0
 while n < 10:
 	a = a + input()
 	n = n + 1
-b = a[0]
+alphabet = a[0]
 for i in a:
-	if i not in b:
-		b = b+i
-print('алфавит: ',b)
+	if i not in alphabet:
+		alphabet = alphabet+i
+print('алфавит: ',alphabet)
