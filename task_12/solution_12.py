953c7be71cfa7618125fae8c6b37eae986ef7f7b
MDAhse <44751798+MDAhse@users.noreply.github.com>
2020-10-29 23:33:47 +0300

    Update solution_12.py

--- task_12/solution_12.py
+++ task_12/solution_12.py
@@ -1,1 +1,11 @@
-# your code here
+line = input()
+occurred_twice = False
+n = 0
+while n < len(line)-1:
+	k =0
+	bi = line[n:n+1]
+	k = line.count(bi)
+	if k >=2:
+		occurred_twice = True
+	n = n + 1
+print(occurred_twice)
